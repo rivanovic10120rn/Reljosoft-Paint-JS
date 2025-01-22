b0d85540acd87ff6b8a5f25238859947d3ff272f
Reljadin <rivanovic10120rn@raf.rs>
2025-01-22 07:16:49 +0100

    Novi floodFill radi optimizovanije dosta, brzi je i resava posao kako treba. Postoji samo mali problem sa strokeovima i outlineovima pri crtanju tako da bi to moglo da se resi ali trenutno deluje da posao radi. OCISTITI KOD OD NEISKORISCENIH FUNKCIJA I KOMENTARA

--- src/paint-app/helperscript.js
+++ src/paint-app/helperscript.js
@@ -169,7 +169,7 @@ const drawStart = (e) => {
     context.fillStyle = selectedColor;
     canvasSnapshot = context.getImageData(0, 0, canvas.width, canvas.height);
     if (selectedTool === "fill") {
-        floodFill(prevMousePoint);
+        floodFill(prevMousePoint, getRGB(selectedColor));
     }
 }
 
@@ -191,7 +191,7 @@ const drawing = (e) => {
         drawRect(position);
     }else if(selectedTool === "circle") {
         drawCircle(position);
-    } else {
+    } else if(selectedTool === "triangle"){
         drawTriangle(position);
     }
     context.stroke();
@@ -274,11 +274,152 @@ canvas.addEventListener("mouseup", drawStop);
 canvas.addEventListener("mouseleave", drawStop);
 canvas.addEventListener("touchend", drawStop);
 
+// NOVI FLOOD FILL
+
+function floodFill(position, color) {
+    console.log(`FloodFill start`);
+    let pixel_stack = [{x: position.x, y: position.y}];
+    let pixels = context.getImageData(0, 0, canvas.width, canvas.height);
+    let linear_cords = (position.y * canvas.width + position.x) * 4;
+    let original_color = {
+        r: pixels.data[linear_cords],
+        g: pixels.data[linear_cords + 1],
+        b: pixels.data[linear_cords + 2],
+        a: 255
+    };
+
+    if (!(color[3] === 255)) {
+        color[3] = 255;
+    }
+
+    if (original_color.r === color[0] &&
+        original_color.g === color[1] &&
+        original_color.b === color[2])
+    {
+        console.log(`First pixel is matching, aborting floodFill`);
+        return;
+    }
+
+    let new_pixel;
+    let x;
+    let y;
+    while (pixel_stack.length > 0) {
+        new_pixel = pixel_stack.shift();
+        x = new_pixel.x;
+        y = new_pixel.y;
+
+        //console.log( x + ", " + y ) ;
 
 
+        linear_cords = (y * canvas.width + x) * 4;
+        while (y-- >= 0 &&
+        (   pixels.data[linear_cords] == original_color.r &&
+            pixels.data[linear_cords + 1] == original_color.g &&
+            pixels.data[linear_cords + 2] == original_color.b)) {
+            linear_cords -= canvas.width * 4;
+        }
+        linear_cords += canvas.width * 4;
+        y++;
 
-// Flood fill function za Fill Color tool
-function floodFill(position) {
+        let reached_left = false;
+        let reached_right = false;
+        while (y++ < canvas.height &&
+        (   pixels.data[linear_cords] == original_color.r &&
+            pixels.data[linear_cords + 1] == original_color.g &&
+            pixels.data[linear_cords + 2] == original_color.b))
+        {
+            pixels.data[linear_cords] = color[0];
+            pixels.data[linear_cords + 1] = color[1];
+            pixels.data[linear_cords + 2] = color[2];
+
+            if (x > 0) {
+                if (pixels.data[linear_cords - 4] == original_color.r &&
+                    pixels.data[linear_cords - 4 + 1] == original_color.g &&
+                    pixels.data[linear_cords - 4 + 2] == original_color.b) {
+                    if (!reached_left) {
+                        // if(is_in_pixel_stack(x - 1, y, pixel_stack)){
+                        //     pixel_stack.push({x: x - 1, y: y});
+                        //     reached_left = true;
+                        // }
+                        pixel_stack.push({x: x - 1, y: y});
+                        reached_left = true;
+                    }
+                } else if (reached_left) {
+                    reached_left = false;
+                }
+            }
+
+            if (x < canvas.width - 1) {
+                if (pixels.data[linear_cords + 4] == original_color.r &&
+                    pixels.data[linear_cords + 4 + 1] == original_color.g &&
+                    pixels.data[linear_cords + 4 + 2] == original_color.b) {
+                    if (!reached_right) {
+                        // if(is_in_pixel_stack(x + 1, y, pixel_stack)) {
+                        //     pixel_stack.push({x: x + 1, y: y});
+                        //     reached_right = true;
+                        // }
+                        pixel_stack.push({x: x + 1, y: y});
+                        reached_right = true;
+                    }
+                } else if (reached_right) {
+                    reached_right = false;
+                }
+            }
+
+            linear_cords += canvas.width * 4;
+        }
+    }
+    context.putImageData( pixels, 0, 0 ) ;
+}
+
+function is_in_pixel_stack( x, y, pixel_stack ) {
+    for(let i=0 ; i<pixel_stack.length ; i++ ) {
+        if( pixel_stack[i].x==x && pixel_stack[i].y==y ) {
+            return true ;
+        }
+    }
+    return false ;
+}
+
+function generate_random_color() {
+    var letters = '0123456789ABCDEF' ;
+    var color = '#' ;
+    for( var i=0; i<6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)] ;
+    }
+    return color ;
+}
+
+// adapted from https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function color_to_rgba( color ) {
+    if( color[0]=="#" ) { // hex notation
+        color = color.replace( "#", "" ) ;
+        var bigint = parseInt(color, 16);
+        var r = (bigint >> 16) & 255;
+        var g = (bigint >> 8) & 255;
+        var b = bigint & 255;
+        return {r:r,
+            g:g,
+            b:b,
+            a:255} ;
+    } else if( color.indexOf("rgba(")==0 ) { // already in rgba notation
+        color = color.replace( "rgba(", "" ).replace( " ", "" ).replace( ")", "" ).split( "," ) ;
+        return {r:color[0],
+            g:color[1],
+            b:color[2],
+            a:color[3]*255} ;
+    } else {
+        console.error( "warning: can't convert color to rgba: " + color ) ;
+        return {r:0,
+            g:0,
+            b:0,
+            a:0} ;
+    }
+}
+
+
+// Stari Flood fill function za Fill Color tool
+function stariFloodFill(position) {
     const imageData = context.getImageData(0,0, canvas.width,canvas.height);
     console.log(context.getImageData(0, 0, canvas.width, canvas.height));
     const data = imageData.data;
@@ -338,7 +479,7 @@ function floodFill(position) {
 
     }
 
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    // context.clearRect(0, 0, canvas.width, canvas.height);
     context.putImageData(imageData, 0, 0);
 }
 
@@ -350,7 +491,7 @@ function getPixelColor (data, x, y) {
     const a = data[index + 3];
 
     console.log(`Pixel at (${x}, ${y}):`, [r,g,b,a]);
-    if(a === 0) return [r,g,b,255];
+    if(a < 255) return [r,g,b,255];
     return [r,g,b,a];
 }
 
