562e417b642dd38dba74efa602246756793d5a9c
Reljadin <rivanovic10120rn@raf.rs>
2025-12-06 03:34:03 +0100

    NEPRAVILNO uradjen Selection Tool ali mislim da vidim gresku.

--- src/paint-app/helperscript.js
+++ src/paint-app/helperscript.js
@@ -190,7 +190,7 @@ const drawing = (e) => {
 
     if (selectedTool === "brush" || selectedTool === "eraser") {
         context.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
-        context.lineTo(position.x, position.y);
+        context.lineTo(Math.round(position.x), Math.round(position.y));
         context.stroke();
     } else if(selectedTool === "line") {
         drawLine(position);
@@ -287,6 +287,17 @@ canvas.addEventListener("touchend", drawStop);
 
 // NOVI FLOOD FILL
 function floodFill(position, color) {
+    let tolerance = 100; //pokusati sve izmedju 5 i 20
+
+    //funkcija unutar funkcije jer pucam vise
+    function matchesColor(idx) {
+        let dr = pixels.data[idx]     - originalColor.r;
+        let dg = pixels.data[idx + 1] - originalColor.g;
+        let db = pixels.data[idx + 2] - originalColor.b;
+        let da = pixels.data[idx + 3] - originalColor.a;
+        return (dr*dr + dg*dg + db*db + da*da) <= tolerance * tolerance;
+    }
+
     let pixelStack = [{x: position.x, y: position.y}];
     let pixels = context.getImageData(0, 0, canvas.width, canvas.height);
     let linearCoordinates = (position.y * canvas.width + position.x) * 4;
@@ -294,18 +305,30 @@ function floodFill(position, color) {
         r: pixels.data[linearCoordinates],
         g: pixels.data[linearCoordinates + 1],
         b: pixels.data[linearCoordinates + 2],
-        a: 255
+        a: pixels.data[linearCoordinates + 3] = 255 //samo settuj na 255 ako treba
     };
 
-    if (!(color[3] === 255)) {
-        color[3] = 255;
-    }
+    // if (!(color[3] === 255)) {
+    //     color[3] = 255;
+    // }
 
-    if (originalColor.r === color[0] &&
-        originalColor.g === color[1] &&
-        originalColor.b === color[2])
-    {
-        console.log(`First pixel is matching, aborting floodFill`);
+    color[3] = 255; //necu vise ni da ga pitam samo bre forceujemo alpha!
+
+    // if (originalColor.r === color[0] &&
+    //     originalColor.g === color[1] &&
+    //     originalColor.b === color[2])
+    // {
+    //     console.log(`First pixel is matching, aborting floodFill`);
+    //     return;
+    // }
+
+    //provera preko delte
+    let dr = originalColor.r - color[0];
+    let dg = originalColor.g - color[1];
+    let db = originalColor.b - color[2];
+    let da = originalColor.a - color[3];
+    if ((dr*dr + dg*dg + db*db + da*da) <= tolerance*tolerance) {
+        console.log(`First pixel matches (with tolerance), aborting floodFill`);
         return;
     }
 
@@ -313,17 +336,15 @@ function floodFill(position, color) {
     let x;
     let y;
     while (pixelStack.length > 0) {
-        newPixel = pixelStack.shift();
+        newPixel = pixelStack.pop();
         x = newPixel.x;
         y = newPixel.y;
 
         //console.log( x + ", " + y ) ;
 
         linearCoordinates = (y * canvas.width + x) * 4;
-        while (y-- >= 0 &&
-        (   pixels.data[linearCoordinates] == originalColor.r &&
-            pixels.data[linearCoordinates + 1] == originalColor.g &&
-            pixels.data[linearCoordinates + 2] == originalColor.b)) {
+        while (y-- >= 0 && matchesColor(linearCoordinates))
+        {
             linearCoordinates -= canvas.width * 4;
         }
         linearCoordinates += canvas.width * 4;
@@ -331,19 +352,16 @@ function floodFill(position, color) {
 
         let reachedLeft = false;
         let reachedRight = false;
-        while (y++ < canvas.height &&
-        (   pixels.data[linearCoordinates] == originalColor.r &&
-            pixels.data[linearCoordinates + 1] == originalColor.g &&
-            pixels.data[linearCoordinates + 2] == originalColor.b))
+        while (y++ < canvas.height && matchesColor(linearCoordinates))
         {
             pixels.data[linearCoordinates] = color[0];
             pixels.data[linearCoordinates + 1] = color[1];
             pixels.data[linearCoordinates + 2] = color[2];
+            pixels.data[linearCoordinates + 3] = color[3];
 
             if (x > 0) {
-                if (pixels.data[linearCoordinates - 4] == originalColor.r &&
-                    pixels.data[linearCoordinates - 4 + 1] == originalColor.g &&
-                    pixels.data[linearCoordinates - 4 + 2] == originalColor.b) {
+                let leftBorder = linearCoordinates - 4;
+                if (matchesColor(leftBorder)) {
                     if (!reachedLeft) {
                         // if(isInPixelStack(x - 1, y, pixelStack)){
                         //     pixelStack.push({x: x - 1, y: y});
@@ -358,9 +376,8 @@ function floodFill(position, color) {
             }
 
             if (x < canvas.width - 1) {
-                if (pixels.data[linearCoordinates + 4] == originalColor.r &&
-                    pixels.data[linearCoordinates + 4 + 1] == originalColor.g &&
-                    pixels.data[linearCoordinates + 4 + 2] == originalColor.b) {
+                let rightBorder = linearCoordinates + 4;
+                if (matchesColor(rightBorder)) {
                     if (!reachedRight) {
                         // if(isInPixelStack(x + 1, y, pixelStack)) {
                         //     pixelStack.push({x: x + 1, y: y});
@@ -379,6 +396,18 @@ function floodFill(position, color) {
     }
     context.putImageData( pixels, 0, 0 ) ;
 }
+
+// popravka za hendlovanje antialiasinga - kaze internet da je threshold najbolje resenje za to
+// const TOLERANCE = 8; // adjust
+// const TOLERANCE_SQUARED = TOLERANCE * TOLERANCE;
+//
+// function matchesColor(i) {
+//     let dr = pixels.data[i]   - originalColor.r;
+//     let dg = pixels.data[i+1] - originalColor.g;
+//     let db = pixels.data[i+2] - originalColor.b;
+//     return (dr*dr + dg*dg + db*db) <= TOLERANCE_SQUARED;
+// }
+//samo cu ubaciti funkciju unutar funkcije jer me ubija ovo prebacivanje parametara
 
 function isInPixelStack( x, y, pixelStack ) {
     for(let i=0 ; i<pixelStack.length ; i++ ) {
@@ -569,3 +598,302 @@ function setupImageUploader() {
         }
     });
 }
+
+//SELECTION ZONE BABY
+
+let selectionMode = "regular"; // regular | lasso
+let isMouseDown = false;
+let selectionX = 0;
+let selectionY = 0;
+let canvasBackup = null;
+let activeSelection = null; // stores selection info
+let lassoPoints = [];
+
+// Selection data structure:
+// {x, y, w, h, imageData, dragging, offsetX, offsetY, resizing, resizeHandle}
+
+canvas.addEventListener("mousedown", startSelection);
+canvas.addEventListener("mousemove", drawSelection);
+canvas.addEventListener("mouseup", stopSelection);
+
+function startSelection(e) {
+    const selectionRectangle = canvas.getBoundingClientRect();
+    selectionX = e.clientX - selectionRectangle.left;
+    selectionY = e.clientY - selectionRectangle.right;
+    isMouseDown = true;
+    canvasBackup = context.getImageData(0,0, canvas.width, canvas.height);
+
+    if (activeSelection) {
+        const handle = isOverResizeHandle(selectionX, selectionY);
+        if(handle) {
+            activeSelection.resizing = true;
+            activeSelection.resizeHandle = handle;
+            return;
+        }
+
+        if (insideSelection(selectionX, selectionY)){
+            activeSelection.dragging = true;
+            activeSelection.offsetX = selectionX - activeSelection.x;
+            activeSelection.offsetY = selectionY - activeSelection.y;
+            return;
+        }
+
+        activeSelection = null;
+    }
+
+    if (selectionMode === "regular") {
+        activeSelection = {x: selectionX, y: selectionY, width: 0, height: 0};
+    }
+
+    if (selectionMode === "lasso") {
+        lassoPoints = [{x: selectionX, y: selectionY}];
+    }
+}
+
+function drawSelection(e) {
+    if(!isMouseDown) return;
+    const rectangle = canvas.getBoundingClientRect();
+    const x = e.clientX - rectangle.left;
+    const y = e.clientY - rectangle.top;
+
+    if(activeSelection?.dragging){
+        activeSelection.x = x - activeSelection.offsetX;
+        activeSelection.y = y - activeSelection.offsetY;
+        redrawSelection();
+        return;
+    }
+
+    if(activeSelection?.resizing){
+        resizeSelection(x,y);
+        redrawSelection();
+        return;
+    }
+
+    context.putImageData(canvasBackup, 0, 0);
+    if (selectionMode === "regular") {
+        activeSelection.width = x - selectionX;
+        activeSelection.height = y - selectionY;
+        drawRectOutline(activeSelection);
+    }
+    if (selectionMode === "lasso") {
+        lassoPoints.push({x,y});
+        drawLassoOutline();
+    }
+}
+
+function stopSelection(e) {
+    isMouseDown = false;
+    if (selectionMode === "regular") finalizeRectSelection();
+    if (selectionMode === "lasso") finalizeLassoSelection();
+}
+
+// RECTANGLE SELECT (EL NORMAL) BBY
+
+function drawRectOutline(selection){
+    context.strokeStyle = "black";
+    context.setLineDash([6,3]);
+    context.strokeRect(selection.x, selection.y, selection.width, selection.height);
+}
+
+function finalizeRectSelection() {
+    const selection = fixRect(activeSelection);
+    activeSelection = selection;
+    activeSelection.imageData = context.getImageData(selection.x, selection.y, selection.width, selection.height);
+    redrawSelection();
+}
+
+// LASSO SELECT BABY (ubicu se)
+
+function drawLassoOutline() {
+    context.strokeStyle = "black";
+    context.setLineDash([6,3]);
+    context.beginPath();
+    lassoPoints.forEach((p, i) => {
+        if (i === 0) context.moveTo(p.x, p.y);
+        else context.lineTo(p.x, p.y);
+    });
+    context.stroke();
+}
+
+function finalizeLassoSelection() {
+    context.putImageData(canvasBackup, 0, 0);
+    const bounds = getLassoBounds();
+    const temporaryCanvas = document.createElement("canvas");
+    temporaryCanvas.width = bounds.width;
+    temporaryCanvas.height = bounds.height;
+    const temporaryContext = temporaryCanvas.getContext("2d");
+    temporaryContext.putImageData(context.getImageData(bounds.x,bounds.y,bounds.width,bounds.height), 0 , 0);
+
+    activeSelection = {
+        x: bounds.x,
+        y: bounds.y,
+        width: bounds.width,
+        height: bounds.height,
+        imageData: temporaryContext.getImageData(0,0,bounds.width,bounds.height),
+    };
+
+    redrawSelection();
+}
+
+function getLassoBounds() {
+    const xs = lassoPoints.map(p => p.x);
+    const ys = lassoPoints.map(p => p.y);
+    return {
+        x: Math.min(...xs),
+        y: Math.min(...ys),
+        width: Math.max(...xs) - Math.min(...xs),
+        height: Math.max(...ys) - Math.min(...ys),
+    };
+}
+
+// DRAG & MOVE BABY
+
+function insideSelection(x,y) {
+    const selection = activeSelection;
+    return x >= selection.x && x <= selection.x + selection.width && y >= selection.y && y <= selection.y + selection.height;
+}
+
+// RESIZE HANDLES BABY
+
+function drawResizeHandles(selection){
+    const size = 8;
+    const handles = getResizeHandles(selection);
+
+    context.fillStyle = "white";
+    context.strokeStyle = "black";
+    handles.forEach(i => {
+        context.fillRect(i.x - size/2, i.y - size/2, size, size);
+        context.strokeRect(i.x - size/2, i.y - size/2, size, size);
+    });
+}
+
+function getResizeHandles(selected){
+    return[
+        {name: "topleft", x: selected.x, y: selected.y},
+        {name: "topright", x: selected.x + selected.width , y: selected.y},
+        {name: "bottomleft", x: selected.x, y: selected.y + selected.height },
+        {name: "bottomright", x: selected.x + selected.width, y: selected.y + selected.height},
+    ];
+}
+
+function isOverResizeHandle(x,y) {
+    const handles = getResizeHandles(activeSelection);
+    const size = 10;
+
+    for (let i of handles) {
+        if (x >= i.x - size && x <= i.x + size && y >= i.y - size && y <= i.y){
+            return i.name;
+        }
+    }
+    return null;
+}
+
+function resizeSelection(mx, my) {
+    const selection = activeSelection;
+    const old = { ...s };
+
+    if (selection.resizeHandle === "bottomright") {
+        selection.width = mx - selection.x;
+        selection.height = my - selection.y;
+    }
+    if (selection.resizeHandle === "topright") {
+        selection.height = old.height + (old.height - my);
+        selection.y = my;
+        selection.width = mx - old.x;
+    }
+    if (selection.resizeHandle === "bottomleft") {
+        selection.width = old.width + (old.x - mx);
+        selection.x = mx;
+        selection.height = my - old.y;
+    }
+    if (selection.resizeHandle === "topleft") {
+        selection.width = old.width + (old.x - mx);
+        selection.x = mx;
+        selection.height = old.height + (old.y - my);
+        selection.y = my;
+    }
+}
+
+// COPY PASTE (DELETE MOZDA?)
+
+function copySelection(){
+    return activeSelection ? activeSelection.imageData : null;
+}
+
+function pasteSelection(data) {
+    if (!data) return;
+    activeSelection = {
+        x: 20,
+        y: 20,
+        width: data.width,
+        height: data.height,
+        imageData: data,
+    };
+    redrawSelection();
+}
+
+function deleteSelection(){
+    if(!activeSelection) return;
+    context.clearRect(activeSelection.x, activeSelection.y, activeSelection.width, activeSelection.height);
+    activeSelection = null;
+}
+
+// RAZONODA BEBO
+
+function redrawSelection() {
+    context.putImageData(canvasBackup, 0, 0);
+    if(!activeSelection) return;
+
+    context.putImageData(activeSelection.imageData, activeSelection.x, activeSelection.y);
+    drawRectOutline(activeSelection);
+    drawResizeHandles(activeSelection);
+}
+
+function fixRect(selection) {
+    let {x,y,width,height} = selection;
+    if (width<0) {
+        x += width;
+        width =Math.abs(width);
+    }
+    if (height<0) {
+        y += height;
+        height = Math.abs(height);
+    }
+    return {x,y,width,height};
+}
+
+//PRONADJENO NA NETU
+// --- Keyboard Shortcuts ---
+// Add this near the bottom of your script, after selection logic is initialized
+window.addEventListener('keydown', (e) => {
+    if (e.ctrlKey && e.key.toLowerCase() === 'c') {
+// CTRL + C => Copy
+        if (selection && selection.active) {
+            copySelection();
+            console.log('Copied selection');
+        }
+        e.preventDefault();
+    }
+    if (e.ctrlKey && e.key.toLowerCase() === 'v') {
+// CTRL + V => Paste
+        if (clipboard) {
+            pasteSelection();
+            console.log('Pasted selection');
+        }
+        e.preventDefault();
+    }
+    if (e.key === 'Delete' || e.key === 'Backspace') {
+// DELETE => Remove selection
+        if (selection && selection.active) {
+            deleteSelection();
+            console.log('Deleted selection');
+        }
+        e.preventDefault();
+    }
+    if (e.key === 'Escape') {
+// ESC => Clear selection
+        deleteSelection(); //clearSelection();
+        console.log('Selection cancelled');
+        e.preventDefault();
+    }
+});
